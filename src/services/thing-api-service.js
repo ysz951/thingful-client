3ebe847f039ac7bd99e5e797817d2a8b20267074
ysz951 <shengyang_zhou@baylor.edu>
2020-08-05 15:31:50 -0500

    add login, private only, public only

--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -1,9 +1,10 @@
 import config from '../config'
-
+import TokenService from '../services/token-service'
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
     })
       .then(res =>
@@ -15,6 +16,7 @@ const ThingApiService = {
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
     })
       .then(res =>
@@ -26,6 +28,7 @@ const ThingApiService = {
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
     })
       .then(res =>
@@ -38,6 +41,7 @@ const ThingApiService = {
     return fetch(`${config.API_ENDPOINT}/reviews`, {
       method: 'POST',
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
         'content-type': 'application/json',
       },
       body: JSON.stringify({
